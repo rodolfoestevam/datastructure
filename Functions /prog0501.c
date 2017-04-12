dfc0a515e0def6e614d9ea2e85811790c36f7f12
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2017-04-12 19:37:52 -0300

    function prog0501

---
+++ Functions /prog0501.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+/*Escrita do Cabeçalho */
+    for ( i = 1; i <=20; i++)
+      putchar('*');
+      putchar('\n');
+
+      puts("Numeros entre 1 e 5: ");
+
+      for (i = 1; i <=20; i++)
+        putchar('*');
+        putchar('\n');
+/*Escrita dos No s */
+      for (i = 1; i < 5; i++)
+      printf("%d\n", i);
+
+      for (i = 1; i < 20; i++)
+      putchar('*');
+      putchar('\n');
+    }
