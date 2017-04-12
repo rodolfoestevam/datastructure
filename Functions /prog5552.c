b7db1296a20a6a2b14b092077373a0da7f3776d9
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2017-04-12 20:37:53 -0300

    FUNC2

---
+++ Functions /prog5552.c
@@ -0,0 +1,17 @@
+      #include <stdio.h>
+      /*Escreva um programa que, reccorendo as tres funcoes distintas. */
+
+linha (int num){
+int i;
+for (i = 1; i <num; i++)
+{
+    putchar('*');
+      putchar('\n');
+}
+}
+int main() {
+  linha(3);
+  linha(5);
+  linha(7);
+
+}
