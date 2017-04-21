7a46cedeea0717b48e34fea906994679d4e0d02e
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2017-04-21 12:31:15 -0300

    size of data types

---
+++ Review/sizeof.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+  printf("O valor do short: %d bytes\n", sizeof(short));
+  printf("O valor do int: %d bytes\n", sizeof(int));
+  printf("O valor do long: %d bytes\n", sizeof(long));
+  return 0;
+}
