17c5bc1656f52e38620e5027be4a32eb352520eb
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2017-05-17 20:11:14 -0300

    binary trees and dynamic allocation exercises

---
+++ binary trees/bt01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fat (int n) {
+ if (n)
+   return n*fat (n-1);
+   else
+   return 1;
+ }
+
+ int main() {
+   int n;
+   printf("\n \nDigite um valor para N: ");
+   scanf("%d", &n);
+   printf("\nO fatorial de %d e %d ", n, fat(n));
+   return 0;
+ }
